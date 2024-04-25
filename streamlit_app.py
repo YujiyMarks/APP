300b149fd58461f823953783a244d0dd48d278ed
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-25 15:14:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.sidebar.title("Menu")
 
 st.title("Relatório de inspeção visual CS")
 st.selectbox("Box",["Opção1","Opção2"])
+st.file_uploader("Insira uma imagem")
+st.radio("Análise")
+st.date_input("Data de Elaboração")
