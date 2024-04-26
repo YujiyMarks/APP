dad57ce5ec250b50dd26c6a86dbc15c0ed761eb4
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-26 09:51:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit as st
 st.sidebar.title("Menu")
 
 st.title("Relatório de inspeção visual CS")
+
 st.selectbox("Box",["Opção1","Opção2"])
+
 st.file_uploader("Insira uma imagem")
-st.radio("Análise")
-st.date_input("Data de Elaboração")
+#st.radio("Análise")
+#st.date_input("Data de Elaboração")
