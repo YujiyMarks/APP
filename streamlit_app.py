de1690499ebabc7b1623d8125056901391ca4b0f
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-26 09:58:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,15 @@ import streamlit as st
 st.sidebar.title("Menu")
 
 st.title("Relatório de inspeção visual CS")
-
 st.selectbox("UFV:",["Campo Verde I","Samambaia"])
-
 st.selectbox("Cliente:",["Alsol","Reenergisa"])
-
 st.selectbox("Responsável:",["João","Geraldo"])
-
 st.selectbox("Revisor:",["João","Geraldo"])
-
-st.date_input("Data de Elaboração")
-
-st.file_uploader("Insira uma imagem")
+st.date_input("Data de elaboração:")
+st.file_uploader("Insira uma imagem:")
 
 #st.radio("Análise")
+
+st.write("ITENS")
+st.selectbox("",["Módulos","Inversores"])
 
