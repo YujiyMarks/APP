b79ac368df7cfcdc4c8fb1f17dbf1ea1a0560724
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-26 10:21:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ st.file_uploader("Insira uma imagem:")
 st.write("ITENS")
 st.selectbox("Escolha o item 1:",["Módulos","Inversores"])
 st.file_uploader("Insira a imagem do item 1:")
-st.radio("Análise:",["C","NC"],horizontal="True")
+st.radio("Análise:",["C","NC"],horizontal=True)
 st.text_input("Observação:")
 
