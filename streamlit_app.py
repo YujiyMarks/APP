40ddad32093ccb5edfeb87c65c6fe82bb98be5be
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-29 11:30:55 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+def inserir_item():
+  st.selectbox("Escolha o item 1:",["Módulos","Inversores"])
+  st.file_uploader("Insira a imagem do item 1:")
+  st.radio("Análise:",["C","NC","NA","PA"],horizontal=True)
+  st.text_input("Observação:")
+
 st.sidebar.title("Menu")
 
 st.title("Relatório de Inspeção Visual CS")
@@ -10,11 +16,7 @@ st.selectbox("Revisor:",["João","Geraldo"])
 st.date_input("Data de elaboração:")
 st.file_uploader("Insira uma imagem:")
 
-st.button("Insira um item",on_click=None)
-
+st.button("Insira um item",on_click=inserir_item())
 st.write("ITENS")
-st.selectbox("Escolha o item 1:",["Módulos","Inversores"])
-st.file_uploader("Insira a imagem do item 1:")
-st.radio("Análise:",["C","NC","NA","PA"],horizontal=True)
-st.text_input("Observação:")
+
 
