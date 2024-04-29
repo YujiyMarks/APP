6b2ef4a394b041802551e57723c6d36c2ab09af1
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-29 11:37:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,24 @@ import streamlit as st
 
 class Pagina():
   def __initi__():
-
-  st.sidebar.title("Menu")
-  
-  st.title("Relatório de Inspeção Visual CS")
-  st.selectbox("UFV:",["Campo Verde I","Samambaia"])
-  st.selectbox("Cliente:",["Alsol","Reenergisa"])
-  st.selectbox("Responsável:",["João","Geraldo"])
-  st.selectbox("Revisor:",["João","Geraldo"])
-  st.date_input("Data de elaboração:")
-  st.file_uploader("Insira uma imagem:")
-  
-  st.write("ITENS")
-  
-  botao = st.button("Insira um item")
-  if botao:
-    st.selectbox("Escolha o item 1:",["Módulos","Inversores"])
-    st.file_uploader("Insira a imagem do item 1:")
-    st.radio("Análise:",["C","NC","NA","PA"],horizontal=True)
-    st.text_input("Observação:")
+    st.sidebar.title("Menu")
+    
+    st.title("Relatório de Inspeção Visual CS")
+    st.selectbox("UFV:",["Campo Verde I","Samambaia"])
+    st.selectbox("Cliente:",["Alsol","Reenergisa"])
+    st.selectbox("Responsável:",["João","Geraldo"])
+    st.selectbox("Revisor:",["João","Geraldo"])
+    st.date_input("Data de elaboração:")
+    st.file_uploader("Insira uma imagem:")
+    
+    st.write("ITENS")
+    
+    botao = st.button("Insira um item")
+    if botao:
+      st.selectbox("Escolha o item 1:",["Módulos","Inversores"])
+      st.file_uploader("Insira a imagem do item 1:")
+      st.radio("Análise:",["C","NC","NA","PA"],horizontal=True)
+      st.text_input("Observação:")
 
 Pagina()
   
