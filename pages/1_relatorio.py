14fb8ea6423a5cafdf0bd014d8d10c4b70dfcdf8
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-04-30 13:11:15 -0300

    Update 1_relatorio.py

--- pages/1_relatorio.py
+++ pages/1_relatorio.py
@@ -1,1 +1,53 @@
+import streamlit as st
 
+#from reportlab.pdfgen import canvas
+#from reportlab.lib.pagesizes import A4
+
+st.set_page_config(page_title="Relatório")
+st.sidebar.header("Relatorio")
+
+class pagina():
+  def __init__(self):
+    st.sidebar.title("Menu")
+    
+    st.title("Relatório de Inspeção Visual CS")
+    st.selectbox("UFV:",["Campo Verde I","Samambaia"])
+    st.selectbox("Cliente:",["Alsol","Reenergisa"])
+    st.selectbox("Responsável:",["João","Geraldo"])
+    st.selectbox("Revisor:",["João","Geraldo"])
+    st.date_input("Data de elaboração:")
+    st.file_uploader("Insira uma imagem:")
+    
+    
+
+  def mostrar_item():
+      st.selectbox("Escolha o item 1:",["Módulos","Inversores"])
+      st.file_uploader("Insira a imagem do item 1:",type=['jpg','png'])
+      st.radio("Análise:",["C","NC","NA","PA"],horizontal=True)
+      st.text_input("Observação:")
+    
+      #botao = st.button("Insira um item")
+      #if botao:
+        #pagina.mostrar_item()
+
+  #def inserir_item():
+    #st.session_state['itens'] += 1
+
+
+
+pagina()
+
+st.write("ITENS")
+botao = st.button("Insira um item")
+if botao:
+  pagina.mostrar_item()
+#botao = False
+#inserir_item(self, botao)
+
+#for i in range(st.session_state['itens']):
+    #mostrar_item()
+
+#progress_bar = st.sidebar.progress(0)
+#status_text = st.sidebar.empty()
+
+#progress_bar.empty()
